8e651f19454baf169a14d91d65a76a46f6577ecf
sujal13-commits <spagade1315@gmail.com>
2026-08-04 02:09:38 +0530

    Force IPv4 for email SMTP connection to fix Render ENETUNREACH error

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -2,7 +2,10 @@ const nodemailer = require('nodemailer');
 
 const sendEmail = async (to, subject, html) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    family: 4, // force IPv4, fixes ENETUNREACH on Render
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
