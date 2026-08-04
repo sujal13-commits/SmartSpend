856c732adecb16441d25bb0b527237e16821df81
sujal13-commits <spagade1315@gmail.com>
2026-08-05 02:29:06 +0530

    Switch from Gmail SMTP to Resend API for reliable email delivery

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,20 +1,10 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendEmail = async (to, subject, html) => {
-  const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com',
-    port: 587,
-    secure: false, // false for port 587 (uses STARTTLS instead of SSL)
-    family: 4,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
-    },
-    connectionTimeout: 10000, // 10 second timeout instead of hanging for 2 minutes
-  });
-
-  await transporter.sendMail({
-    from: `"SmartSpend" <${process.env.EMAIL_USER}>`,
+  await resend.emails.send({
+    from: 'SmartSpend <onboarding@resend.dev>',
     to,
     subject,
     html,
